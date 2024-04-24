900d7009b2d7f8519f8bfde28adf6753c46abf1e
nazmul23 <nazmulahasan.bs23@gmail.com>
2024-04-24 19:05:57 +0600

    Adding files for terraform

---
+++ terraform/terraform-main.tf
@@ -0,0 +1,78 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "fn_bucket" {
+  bucket = "fn-s3-bucket"
+  tags = {
+    Name = "FnS3Bucket"
+  }
+}
+
+resource "aws_s3_bucket_acl" "fn_bucket_acl" {
+  bucket = aws_s3_bucket.fn_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "fn_bucket_vc" {
+  bucket = aws_s3_bucket.fn_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "fn_bucket_lc" {
+  bucket = aws_s3_bucket.fn_bucket.id
+
+  rule {
+    id = "expire"
+
+    expiration {
+      days = 365
+    }
+    status = "Enabled"
+  }
+}
+
+resource "aws_cloudfront_distribution" "fn_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.fn_bucket.bucket_regional_domain_name
+    origin_id   = "fn-s3-bucket"
+
+    s3_origin_config {
+      origin_access_identity = ""
+    }
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = "fn-s3-bucket"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
